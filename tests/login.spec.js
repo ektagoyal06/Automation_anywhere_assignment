665393912c080fe1f01ad245aa7fbe350d198157
Ekta Goyal <160715168+ektagoyal06@users.noreply.github.com>
2025-12-19 17:54:35 +0530

    updated

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,8 +5,9 @@ test('Login to Automation Anywhere', async ({ page }) => {
   const login = new LoginPage(page);
 
   await login.goto();
-  await login.login('ektagoyal6904@gmail.com');
+  await login.login('Your_email');
 
 
   await expect(page).toHaveURL(/automationanywhere/);
 });
+
